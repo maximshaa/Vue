23f29adb0097c186aed81ac5a61ed994e7111fb3
maximshaa <starwwind@gmail.com>
2025-03-11 17:01:55 +0700

    vue4

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ let app = new Vue({
         product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: true
+        inStock: true,
+        inventory: 10,
+        onSale: true,
     }
 })
